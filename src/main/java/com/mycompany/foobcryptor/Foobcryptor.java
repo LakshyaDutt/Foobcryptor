ee59df5efd8325bbc2ca42ec131c9cc08f0bda68
LakshyaDutt <39757914+LakshyaDutt@users.noreply.github.com>
2023-09-12 17:18:02 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/foobcryptor/Foobcryptor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.foobcryptor;
+
+/**
+ *
+ * @author lakshya
+ */
+public class Foobcryptor {
+
+    public static void main(String[] args) {
+        options_page op = new options_page();
+        op.show();
+    }
+}
